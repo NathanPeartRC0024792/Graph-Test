e6b98c7ce1a5b4a2c70cfd60883ca3144a0f87b1
Nathan Peart <RC0024792@student.riversidecollege.ac.uk>
2018-04-18 10:24:26 +0100

    Add project files.

---
+++ Graph Test/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System; using System.Windows.Forms.DataVisualization.Charting;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graph_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            chart1.Series.Clear();
+            chart1.ChartAreas[0].AxisX.IsMarginVisible = false;
+            Series series1 = new Series
+            {
+                Name = "Line",
+                Color = Color.Blue,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = SeriesChartType.Spline,
+                BorderWidth = 2
+            };
+            Series series2 = new Series
+            {
+                Name = "Points",
+                Color = Color.Red,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = SeriesChartType.Point,
+                BorderWidth = 2
+            };
+            chart1.Series.Add(series1);
+            chart1.Series.Add(series2);
+            for (int i = 0; i < 11; i++)
+            {
+                series1.Points.AddXY(i, i * i);
+                series2.Points.AddXY(i, i * i);
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
